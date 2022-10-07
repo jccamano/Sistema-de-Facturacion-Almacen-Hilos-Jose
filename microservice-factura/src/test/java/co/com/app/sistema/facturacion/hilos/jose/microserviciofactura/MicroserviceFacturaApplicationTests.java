e3104d91f193cedc578b85f6ba97e80f32f8f44a
Juan Carlos Camano <jucahe27@gmail.com>
2022-10-06 20:55:12 -0500

    se crea el microservice factura

---
+++ microservice-factura/src/test/java/co/com/app/sistema/facturacion/hilos/jose/microserviciofactura/MicroserviceFacturaApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.app.sistema.facturacion.hilos.jose.microserviciofactura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceFacturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
