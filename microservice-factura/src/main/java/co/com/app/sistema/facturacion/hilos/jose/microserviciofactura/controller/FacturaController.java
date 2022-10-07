aa4876f1d3836f878865a0e526959b4d188aca9d
Juan Carlos Camano <jucahe27@gmail.com>
2022-10-06 22:17:56 -0500

    se crea metodo aditar en la clase controller

--- microservice-factura/src/main/java/co/com/app/sistema/facturacion/hilos/jose/microserviciofactura/controller/FacturaController.java
+++ microservice-factura/src/main/java/co/com/app/sistema/facturacion/hilos/jose/microserviciofactura/controller/FacturaController.java
@@ -10,12 +10,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import co.com.app.sistema.facturacion.hilos.jose.microserviciofactura.models.entity.Factura;
 import co.com.app.sistema.facturacion.hilos.jose.microserviciofactura.service.FacturaService;
 
 @RestController
+@RequestMapping("/factura")
 public class FacturaController {
 
 	@Autowired
