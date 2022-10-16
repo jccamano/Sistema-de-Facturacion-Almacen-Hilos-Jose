bd74be1c579b3886af32632368a1a021c1ff3ceb
Juan Carlos Camano <jucahe27@gmail.com>
2022-10-16 18:18:01 -0500

    se crea el microservicio registry

---
+++ microservice-eureka-server/src/test/java/co/com/app/sistema/facturacion/hilos/jose/microservicioeurekaserver/MicroserviceEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.app.sistema.facturacion.hilos.jose.microservicioeurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
