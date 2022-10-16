72819fd5361a12b87c089f25424f0573220c55ad
Juan Carlos Camano <jucahe27@gmail.com>
2022-10-16 18:17:33 -0500

    se crea el microservicio registry

--- microservice-cliente/src/main/java/co/com/app/sistema/facturacion/hilos/jose/microserviciocliente/MicroserviceClienteApplication.java
+++ microservice-cliente/src/main/java/co/com/app/sistema/facturacion/hilos/jose/microserviciocliente/MicroserviceClienteApplication.java
@@ -2,7 +2,9 @@ package co.com.app.sistema.facturacion.hilos.jose.microserviciocliente;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class MicroserviceClienteApplication {
 
